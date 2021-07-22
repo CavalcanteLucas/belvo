cd8b4d5ed70f8fe7f726961a0d1de8d6d73646f3
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-07-22 18:09:51 -0300

    Implement UserTests, with test_list_users_successfully, and test_create_user_successfully

--- belvo/users/tests.py
+++ belvo/users/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from model_bakery import baker
+from rest_framework import status
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+from belvo.users.models import User
+from belvo.users.serializers import UserSerializer
+
+
+class UserTests(TestCase):
+    def test_list_users_successfully(self):
+        self.assertEqual(0, User.objects.count())
+        user = baker.make("users.User")
+        self.assertEqual(1, User.objects.count())
+
+        url = reverse("users:user")
+        response = self.client.get(path=url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(1, len(response.data))
+
+        expected_data = [
+            {
+                "name": user.name,
+                "email": user.email,
+                "age": user.age,
+            }
+        ]
+        self.assertEqual(expected_data, response.data)
+
+    def test_create_user_successfully(self):
+        self.assertEqual(0, User.objects.count())
+
+        user_sample = baker.prepare("users.User")
+
+        url = reverse("users:user")
+        response = self.client.post(
+            path=url,
+            content_type="application/json",
+            data=UserSerializer(user_sample).data,
+        )
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(1, User.objects.count())
+
+        expected_data = {
+            "name": user_sample.name,
+            "email": user_sample.email,
+            "age": user_sample.age,
+        }
+
+        self.assertEqual(expected_data, response.data)
