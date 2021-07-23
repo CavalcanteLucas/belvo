1b594074f4d34ab67d28d29ae57e295e1eef7d98
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2021-07-22 21:24:27 -0300

    Implement Transaction constraints and TransactionTests

--- belvo/transactions/models.py
+++ belvo/transactions/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.db.models.expressions import CombinedExpression, F
 
 from belvo.users.models import User
 
-TRANSACTION_TYPES = [
-    ("INFLOW", "inflow"),
-    ("OUTFLOW", "outflow"),
-]
+
+class TransactionTypes(models.TextChoices):
+    INFLOW = "IN", "inflow"
+    OUTFLOW = "OU", "outflow"
 
 
 class Transaction(models.Model):
@@ -13,6 +14,21 @@ class Transaction(models.Model):
     account = models.CharField(max_length=6)
     date = models.DateField()
     amount = models.DecimalField(max_digits=8, decimal_places=2)
-    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    type = models.CharField(max_length=7, choices=TransactionTypes.choices)
     category = models.CharField(max_length=20)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="only_two_types_of_transaction",
+                check=models.Q(type__in=TransactionTypes.values),
+            ),
+            models.CheckConstraint(
+                name="inflow_positive_or_outflow_negative",
+                check=(
+                    models.Q(type="IN", amount__gte=0)
+                    | models.Q(type="OU", amount__lt=0)
+                ),
+            ),
+        ]
--- belvo/transactions/tests.py
+++ belvo/transactions/tests.py
@@ -1,10 +1,52 @@
 from model_bakery import baker
 
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
-from belvo.transactions.models import Transaction
+from belvo.transactions.models import Transaction, TransactionTypes
 
 
 class TransactionTests(TestCase):
     def test_that_a_transaction_reference_is_unique(self):
-        return
+        self.assertEqual(0, Transaction.objects.count())
+        transaction_sample = baker.make(
+            "Transaction", type=TransactionTypes.OUTFLOW, amount="-1"
+        )
+        self.assertEqual(1, Transaction.objects.count())
+
+        with self.assertRaises(IntegrityError):
+            baker.make(
+                "Transaction",
+                type=TransactionTypes.OUTFLOW,
+                amount="-2",
+                reference=transaction_sample.reference,
+            )
+            self.assertEqual(1, Transaction.objects.count())
+
+    def test_that_there_are_only_two_types_of_transactions(self):
+        self.assertEqual(0, Transaction.objects.count())
+
+        baker.make("Transaction", type=TransactionTypes.OUTFLOW, amount="-1.99")
+        self.assertEqual(1, Transaction.objects.count())
+
+        baker.make("Transaction", type=TransactionTypes.INFLOW, amount="3.14")
+        self.assertEqual(2, Transaction.objects.count())
+
+        with self.assertRaises(IntegrityError):
+            baker.make("Transaction", type="OTHER")
+
+    def test_that_all_outflow_transactions_amounts_are_negative_decimal_numbers(self):
+        self.assertEqual(0, Transaction.objects.count())
+        baker.make("Transaction", type=TransactionTypes.OUTFLOW, amount="-1.99")
+        self.assertEqual(1, Transaction.objects.count())
+        with self.assertRaises(IntegrityError):
+            baker.make("Transaction", type=TransactionTypes.OUTFLOW, amount="2.00")
+            self.assertEqual(1, Transaction.objects.count())
+
+    def test_that_all_inflow_transactions_amounts_are_positive_decimal_numbers(self):
+        self.assertEqual(0, Transaction.objects.count())
+        baker.make("Transaction", type=TransactionTypes.INFLOW, amount="1.99")
+        self.assertEqual(1, Transaction.objects.count())
+        with self.assertRaises(IntegrityError):
+            baker.make("Transaction", type=TransactionTypes.INFLOW, amount="-0.10")
+            self.assertEqual(1, Transaction.objects.count())
